fce5f0f89030742885f8b1906075137fbde97867
Niccolò <niccolo@niccolo.home>
2017-10-14 18:18:34 +0200

    initial commit

---
+++ src/menus/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+from restaurants.models import RestaurantLocation
+from django.core.urlresolvers import reverse
+
+
+# Create your models here.
+class Item(models.Model):
+	
+	#Association
+	user 		= models.ForeignKey(settings.AUTH_USER_MODEL)
+	restaurant 	= models.ForeignKey(RestaurantLocation)
+	
+	#Item Staff
+	name 		= models.CharField(max_length=120)
+	contents	= models.TextField(help_text='separate each item by comma')
+	excludes 	= models.TextField(blank=True, null=True, help_text='separate each item by comma')
+	public		= models.BooleanField(default=True)
+	timestamp 	= models.DateTimeField(auto_now_add=True)
+	updated 	= models.DateTimeField(auto_now=True)
+	
+	class Meta:
+		ordering = ['-updated', '-timestamp']
+		
+	def __str__(self):
+		return self.name
+
+	def get_contents(self):
+		return self.contents.split(",")
+
+	def get_excludes(self):
+		return self.excludes.split(",")
+	def get_absolute_url(self):
+		#return f('/restaurants/{self.slug}')
+		return reverse('menus:detail', kwargs={"pk":self.pk})
---
+++ src/menus/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from .views import (
+    ItemListView,
+    ItemDetailView,
+    ItemCreateView,
+	ItemUpdateView,
+
+)
+
+urlpatterns = [
+	url(r'^$' , ItemListView.as_view(), name='list'),
+    url(r'^create/$', ItemCreateView.as_view(), name='create'),
+	
+    url(r'^ (?P<pk>\d+)/$', ItemUpdateView .as_view(), name='detail'),
+    #url(r'^ (?P<pk>\d+)/edit/$', ItemUpdateView .as_view(), name='edit'),
+    
+
+    
+    
+]
+  
